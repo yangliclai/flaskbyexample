f16b98d00a952940ac8c1c7cbf55e56d309840a2
yangliclai <yangliclai@gmail.com>
2017-12-15 17:01:03 -0500

    make hello.py back to the correct version including flask module

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
-print("Hello,World!")
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+# print("Hello,World!")
